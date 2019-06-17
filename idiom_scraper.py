b55f7fb20a4611065dd0566285df43d31c5a8b46
Daniel Russotto <russotto.d@husky.neu.edu>
2019-06-17 14:46:30 +0200

    Tagging idioms in sentences

--- idiom_scraper.py
+++ idiom_scraper.py
@@ -7,6 +7,9 @@ Created on Sat Jun 15 13:01:12 2019
 
 import requests
 from bs4 import BeautifulSoup
+import os.path
+
+OUTPUT_FILE = "idioms.txt"
 
 response = requests.get("https://7esl.com/english-idioms/")
 
@@ -15,6 +18,11 @@ soup = BeautifulSoup(response.text, "html.parser")
 idioms = [strong_tag.get_text() for strong_tag in soup.select("td strong")]
 
 print("Number of idioms: {}".format(len(idioms)))
+
+# Clear contents if the file exists
+if os.path.exists(OUTPUT_FILE):
+    with open(OUTPUT_FILE, "w") as f:
+        f.write("")
 
 with open("idioms.txt", "a") as f:
     for idiom in idioms:    
