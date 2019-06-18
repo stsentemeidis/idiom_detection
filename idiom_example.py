eeb5ab77dab5d15c8e3dfbd5982b4d792707d298
Jiachao Pan <jiachao.pan@student.ie.edu>
2019-06-18 20:16:25 +0200

    fix the example data

--- idiom_example.py
+++ idiom_example.py
@@ -3,45 +3,40 @@
 import pandas as pd
 import requests
 import re
+import json
+import unicodedata
 # from utils import clear_file
+from bs4 import BeautifulSoup
 
 OUTPUT_FILE = "idioms.txt"
 
 response = requests.get("https://7esl.com/english-idioms/")
+soup = BeautifulSoup(response.text, "lxml")
+r_text = soup.find_all('li')
+# .get_text(strip=True)
 
-
-
-r_text = response.text.encode()
-r_content = re.findall('<h2><span class=\"ez-toc-section\" id=\"Idiom_Examples\">(.*?)<h2><span class=\"ez-toc-section\" id=\"Common_English_Idioms_Images\">',str(r_text))
-
-li_list = re.findall('<li><em>(.*?)<\/em><\/li>', r_content[0])
-
+# r_text = response.text.encode('utf-8')
 
 idioms = []
 sentences = []
-for l in li_list:
-  if 'Example' not in l:
-    keyword = re.findall('<strong><em>(\w+.*?)<\/em></strong>', str(l))
-
+for l in r_text:
+  if re.search('<li><em>',str(l)):
+#     print(l)
+    keyword = re.findall('<strong>(.*?)<\/strong>',str(l))
     if len(keyword):
-      keyword = re.sub('(<em>|<\/em>|<li>|</li>|<strong>|</strong>|\\\\|x\w\d)','',str(keyword[0]))
-      sentence = re.sub('(<strong>|<strong style=\"font-style: inherit;\">)',' ',str(l))
-      sentence = re.sub('(<em>|<\/em>|<li>|</li>|<strong>|<strong style=\"font-style: inherit;\">|</strong>|\\\\|x\w\d)','',sentence)
-
+      keyword = re.sub('<em>|<\/em>|\(','',keyword[0])
     else:
-      l = re.sub('\\\\x\w\d','',str(l))
-      keyword = re.findall('<strong style=\"font-style: inherit;\"><em>(\w+.*?)<\/em><\/strong>',str(l))
-      if len(keyword):
-        keyword = keyword[0]
-        sentence = re.sub('(<em>|<\/em>|<li>|</li>|<strong>|<strong style=\"font-style: inherit;\">|</strong>|\\\\|x\w\d)','',str(l))
-
-    sentence = re.sub('(\(.*?\))|(<\/ul>.*?)|(<\/p>.*?)|(<p.*?)|(<h3>.*?)','',sentence)
-
+      keyword = re.findall('<strong style=\"font-style: inherit;\">(.*?)</strong>', str(l))
+      keyword = re.sub('<em>|<\/em>|\(','',keyword[0])
+      
+    sentence = re.sub('(<\w+>)|(<\/\w+>)|(\(.*?\))|(<strong style=\"font-style: inherit;\">)','',str(l))
+    sentence = re.sub('(\s+)',' ',sentence)
     idioms.append(keyword)
     sentences.append(sentence)
-
+    print(keyword)
+    print(sentence)
+    print("===============================")
     
-
 example_df = pd.DataFrame({'idiom':idioms, 'sentence':sentences})
 example_df['idiom'] = example_df['idiom'].map(lambda x: re.sub('\(','',str(x)))
-example_df.to_csv('idiom_example.csv')
+example_df.to_csv('/data/idiom_example.csv')
